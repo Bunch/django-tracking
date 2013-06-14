4f2496f5680a63e8edf718355111ee11a82d3c8c
elguntor <elguntor@gmail.com>
2013-06-14 10:28:13 -0400

    added indexes on time to PageVisit and TrackingEvent

---
+++ tracking/migrations/0003_auto.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding index on 'TrackingEvent', fields ['time']
+        db.create_index('tracking_trackingevent', ['time'])
+
+        # Adding index on 'PageVisit', fields ['time']
+        db.create_index('tracking_pagevisit', ['time'])
+
+
+    def backwards(self, orm):
+        # Removing index on 'PageVisit', fields ['time']
+        db.delete_index('tracking_pagevisit', ['time'])
+
+        # Removing index on 'TrackingEvent', fields ['time']
+        db.delete_index('tracking_trackingevent', ['time'])
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'tracking.bannedip': {
+            'Meta': {'ordering': "('ip_address',)", 'object_name': 'BannedIP'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.IPAddressField', [], {'max_length': '15'})
+        },
+        'tracking.pagevisit': {
+            'Meta': {'object_name': 'PageVisit'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'query': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'visitor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tracking.Visitor']"})
+        },
+        'tracking.trackingevent': {
+            'Meta': {'object_name': 'TrackingEvent'},
+            'data': ('django.db.models.fields.BigIntegerField', [], {'null': 'True'}),
+            'event': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'visitor': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tracking.Visitor']"})
+        },
+        'tracking.untrackeduseragent': {
+            'Meta': {'ordering': "('keyword',)", 'object_name': 'UntrackedUserAgent'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'tracking.visitor': {
+            'Meta': {'ordering': "('-last_update',)", 'unique_together': "(('session_key', 'ip_address'),)", 'object_name': 'Visitor'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'last_update': ('django.db.models.fields.DateTimeField', [], {}),
+            'page_views': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'referrer': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'session_key': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'session_start': ('django.db.models.fields.DateTimeField', [], {}),
+            'tid': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'}),
+            'user_agent': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['tracking']
